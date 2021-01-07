7a8ce871b674b5feaf011d1532c3f99f38974c3b
mutsuisan <m.yoshimuta.29s@gmail.com>
2021-01-07 22:26:58 +0900

    fetch access token

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -11,7 +12,8 @@ import (
 )
 
 var (
-	loginDialogURLBase string = "https://www.facebook.com/v9.0/dialog/oauth"
+	loginDialogURLBase    string = "https://www.facebook.com/v9.0/dialog/oauth"
+	getAccessTokenURLBase string = "https://graph.facebook.com/v9.0/oauth/access_token"
 )
 
 func main() {
@@ -19,6 +21,7 @@ func main() {
 
 	var redirectURL = os.Getenv("GOGRAM_REDIRECT_URL")
 	var clientID = os.Getenv("GOGRAM_CLIENT_ID")
+	var clientSecret = os.Getenv("GOGRAM_CLIENT_SECRET")
 	fmt.Println(redirectURL, clientID)
 
 	var loginDialogURL string = fmt.Sprintf("%s?client_id=%s&redirect_uri=%s", loginDialogURLBase, clientID, redirectURL)
@@ -30,7 +33,23 @@ func main() {
 	router.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
 		queries := r.URL.Query()
 		fmt.Fprintf(w, "state: %s\n", strings.Join(queries["state"], ""))
-		fmt.Fprintf(w, "code: %s\n", strings.Join(queries["code"], ""))
+		code := strings.Join(queries["code"], "")
+		fmt.Fprintf(w, "code: %s\n", code)
+		fmt.Fprintln(w, "-----------------------------")
+
+		fmt.Println("fetching access token...")
+		var getAccessTokenURL string = fmt.Sprintf("%s?client_id=%s&redirect_uri=%s&client_secret=%s&code=%s", getAccessTokenURLBase, clientID, redirectURL, clientSecret, code)
+		res, err := http.Get(getAccessTokenURL)
+		if err != nil {
+			panic(err)
+		}
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			panic(err)
+		}
+		bodyStr := string(body)
+		fmt.Fprintf(w, bodyStr)
+		fmt.Fprintln(w, "\n-----------------------------")
 	})
 
 	// login page
